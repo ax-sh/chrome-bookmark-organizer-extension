2db38cd96199bd06e3042a3b659041b4133b7bba
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-01-25 02:19:57 +0545

    refactor: simplify bookmark handling by importing utility

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,29 +1,4 @@
-interface BookmarkNode {
-  children?: BookmarkNode[];
-  url?: string;
-  title: string;
-  id: string;
-}
-
-type BM = Pick<BookmarkNode, 'id' | 'url'>;
-
-function traverseBookmarks(node: BookmarkNode) {
-  let urls: BM[] = [];
-
-  // If the node has a URL, it's a bookmark (leaf node)
-  if (node.url) {
-    urls.push({ url: node.url, id: node.id });
-  }
-
-  // If the node has children (folder), recursively process them
-  if (node.children) {
-    for (const child of node.children) {
-      urls = urls.concat(traverseBookmarks(child));
-    }
-  }
-
-  return urls;
-}
+import { traverseBookmarks } from './utils';
 
 async function readBookmarks() {
   const bookmarks = await chrome.bookmarks.getTree();
