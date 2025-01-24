e822e36991cdf689ceace39ec178a9eb3603defb
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-01-25 02:29:07 +0545

    feat(utils): add title to bookmark type and traversal function

--- entrypoints/utils/index.ts
+++ entrypoints/utils/index.ts
@@ -1,10 +1,10 @@
-export type BM = Pick<chrome.bookmarks.BookmarkTreeNode, 'id' | 'url'>;
+export type BM = Pick<chrome.bookmarks.BookmarkTreeNode, 'id' | 'url' | 'title'>;
 
 export function traverseBookmarks(node: chrome.bookmarks.BookmarkTreeNode): BM[] {
   let urls: BM[] = [];
 
   if (node.url) {
-    urls.push({ url: node.url, id: node.id });
+    urls.push({ url: node.url, id: node.id , title: node.title});
   }
 
   if (node.children) {
