0e2efe87abdf5501187e690078d70145fa012285
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-01-25 02:53:56 +0545

    feat: integrate Tailwind CSS and simplify App component

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,4 +1,5 @@
 import reactLogo from '@/assets/react.svg';
+import BookmarksTable from '@/components/bookmarks-table';
 import { BM, readBookmarks } from '@/entrypoints/utils';
 import { useEffect, useState } from 'react';
 
@@ -18,24 +19,9 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div className='bg-red-500 w-full'>
-        <a href='https://wxt.dev' target='_blank'>
-          <img src={wxtLogo} className='logo' alt='WXT logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>WXT + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      {/* <pre>{JSON.stringify(bookmarks, null, 2)}</pre> */}
-    </>
+    <div className='bg-red-500 w-full'>
+      <BookmarksTable data={bookmarks} />
+    </div>
   );
 }
 
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
@@ -7,10 +8,16 @@ export default defineConfig({
   manifest: {
     permissions: ['bookmarks'],
   },
+  vite: () => ({
+    plugins: [tailwindcss()],
+    // Override config here, same as `defineConfig({ ... })`
+    // inside vite.config.ts files
+  }),
   runner: {
     // chromiumArgs: ['--user-data-dir=./.wxt/chrome-data'],
     openDevtools: true,
     openConsole: true,
+    disabled: true,
     binaries: {
       // chrome: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
       chrome: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
