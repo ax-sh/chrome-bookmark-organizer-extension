152434e425cce4c7e321b577b5f16b9e9743bebc
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-01-25 01:48:14 +0545

    feat: add bookmark reading functionality in background script

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,9 @@
-export default defineBackground(() => {
+async function readBookmarks() {
+  const bookmarks = await chrome.bookmarks.getTree()
+  console.table(bookmarks)
+}
+
+export default defineBackground(async () => {
   console.log('Hello background!', { id: browser.runtime.id });
+ await readBookmarks()
 });
