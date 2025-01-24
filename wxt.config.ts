df7f1a66c1643b5c4e90c90e8f18965aa1be3087
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-01-25 03:05:11 +0545

    chore: comment out tailwind imports and update config

--- wxt.config.ts
+++ wxt.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     // chromiumArgs: ['--user-data-dir=./.wxt/chrome-data'],
     openDevtools: true,
     openConsole: true,
-    disabled: true,
+    // disabled: true,s
     binaries: {
       // chrome: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
       chrome: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
