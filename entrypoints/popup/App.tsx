5fdc5c3e707035693566eacef6ad76b87a0619f7
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-01-25 15:57:03 +0545

    fix: correct type annotation in SearchInput component

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 
 import wxtLogo from '/wxt.svg';
 
-function SearchInput(props: { value: string; onChange: (e) => void }) {
+function SearchInput(props: { value: string; onChange: (e: string) => void }) {
   return (
     <input
       type='search'
