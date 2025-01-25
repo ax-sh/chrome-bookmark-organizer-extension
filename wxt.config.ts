54214a5c9512540feb3882011668aca709812993
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-01-26 03:02:38 +0545

    refactor: update manifest to use UserManifest type

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,12 +1,17 @@
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'wxt';
+import { type UserManifest, defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react'],
-  manifest: {
-    permissions: ['bookmarks'],
+  manifest: ({ browser }) => {
+    const config: UserManifest = {
+      permissions: ['bookmarks'],
+    };
+    console.log(browser, '<Manifest');
+    console.log(config);
+    return config;
   },
   vite: () => ({
     plugins: [tailwindcss()],
